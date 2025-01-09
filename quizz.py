17093f4bd22e976ce316ca999741414d50f3dd2a
rodrigocc2 <rcarvalhoben10@gmail.com>
2025-01-09 20:01:33 +0000

    Add files via upload

---
+++ quizz.py
@@ -0,0 +1,74 @@
+from tabnanny import verbose
+
+pergunta1=int(input("pergunta/n quanto e 2+2/2 opcoes 3 2 "))
+if pergunta1==3:
+    print("se voce responder 3 voce ganhou 10 pontos")
+else:
+    print("se voce responder 2 voce não ganhou nenhum ponto")
+
+pergunta2=int(input("pergunta/n quanto e 4+4x3-5 opcoes 19 11"))
+if pergunta2==11:
+    print("se voce responder 11 voce ganhou 10 pontos")
+else:
+    print("se voce responder 19 voce não ganhou nenhum ponto")
+
+pergunta3=int(input("pergunta/n como se escreve nine em portugues 9 8"))
+if pergunta3==9:
+    print("se voce respnder 9 voce ganhou 10 pontos")
+else:
+    print("se voce responder 8 voce ganhou nenhum ponto")
+
+pergunta4=int(input("pergunta/n como se escreve four em portugues 4 5"))
+if pergunta4==4:
+    print("se voce responder 4 voce ganhou 10 pontos")
+else:
+    print("se voce responder 5 voce ganhou nenhum ponto ")
+
+pergunta5=int(input("pergunata/n qual e o tamanho de uma girafa em media opcoes 6 7"))
+if pergunta5==6:
+    print("se voce responder 6 voce ganhou 10 pontos ")
+else:
+    print("se voce responder 7 voce ganhou 0 pontos")
+
+pergunta6=int(input("quantas toneladas pesa em media um elefate opcoes 6 5"))
+if pergunta6==5:
+    print("se responnderes 5 ganhas 10 pontos")
+else:
+    print("se responderes 6 ganhas 0 pontos")
+
+pergunta7=int(input("quantos golos tem o cristiano ronaldo 919 912"))
+if pergunta7==919:
+    print("se responderes 919 voce ganhou 10 pontos")
+else:
+    print("se responderes 912 voce ganhou 0 pontos")
+
+pergunta8=int(input("qual e a temperatura de mercurio opcoes 126 178"))
+if pergunta8==126:
+    print("se responderes 126 voce ganhou 10 pontos")
+else:
+    print("se voce responder 178 voce ganhou 0 pontos")
+
+pergunta9=int(input("quantos anos tem o antonio costa opcoes 67 63"))
+if pergunta9==63:
+    print("se voce respondeu 63 voce ganhou 10 pontos")
+else:
+     print("se voce respondeu 67 voce ganhou 0 pontos")
+
+pergunta10=int(input("quantos kilometros arredondados tem a terra opcoes 40000 3892"))
+if pergunta10==40000:
+    print("se voce respondeu 40000 voce ganhou 10 pontos")
+else:
+    print("se voce respondeu 3892 voce ganhou 0 pontos ")
+
+
+
+
+
+
+
+
+
+
+
+
+
